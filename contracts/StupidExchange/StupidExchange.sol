260aee43691aef5b4777745ad30b52a02bfe94e9
alexintosh <alessio.d@gmail.com>
2018-11-29 12:09:39 +0100

    Adding contracts folder

---
+++ contracts/StupidExchange/StupidExchange.sol
@@ -0,0 +1,241 @@
+pragma solidity 0.4.25;
+
+contract Ownable {
+    address private _owner;
+
+    event OwnershipTransferred(
+        address indexed previousOwner,
+        address indexed newOwner
+    );
+
+    /**
+    * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+    * account.
+    */
+    constructor() public {
+        _owner = msg.sender;
+        emit OwnershipTransferred(address(0), _owner);
+    }
+
+    /**
+    * @return the address of the owner.
+    */
+    function owner() public view returns(address) {
+        return _owner;
+    }
+
+    /**
+    * @dev Throws if called by any account other than the owner.
+    */
+    modifier onlyOwner() {
+        require(isOwner());
+        _;
+    }
+
+    /**
+    * @return true if `msg.sender` is the owner of the contract.
+    */
+    function isOwner() public view returns(bool) {
+        return msg.sender == _owner;
+    }
+
+    /**
+    * @dev Allows the current owner to relinquish control of the contract.
+    * @notice Renouncing to ownership will leave the contract without an owner.
+    * It will not be possible to call the functions with the `onlyOwner`
+    * modifier anymore.
+    */
+    function renounceOwnership() public onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+
+    /**
+    * @dev Allows the current owner to transfer control of the contract to a newOwner.
+    * @param newOwner The address to transfer ownership to.
+    */
+    function transferOwnership(address newOwner) public onlyOwner {
+        _transferOwnership(newOwner);
+    }
+
+    /**
+    * @dev Transfers control of the contract to a newOwner.
+    * @param newOwner The address to transfer ownership to.
+    */
+    function _transferOwnership(address newOwner) internal {
+        require(newOwner != address(0));
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+}
+
+library SafeMath {
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+        uint256 c = a * b;
+        assert(c / a == b);
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
+}
+
+interface ERC20Interface {
+    function totalSupply() public constant returns (uint);
+    function balanceOf(address tokenOwner) public constant returns (uint balance);
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
+    function transfer(address to, uint tokens) public returns (bool success);
+    function approve(address spender, uint tokens) public returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
+
+contract StupidExchange is Ownable {
+    using SafeMath for uint256;
+
+    /// EVENTS
+    event EthToTokenPurchase(address indexed buyer, uint256 indexed ethIn, uint256 indexed tokensOut);
+    event TokenToEthPurchase(address indexed buyer, uint256 indexed tokensIn, uint256 indexed ethOut);
+
+    /// CONSTANTS
+    uint256 public constant FEE_RATE = 500;        //fee = 1/feeRate = 0.2%
+
+    /// STORAGE
+    uint256 public ethPool;
+    uint256 public tokenPool;
+    uint256 public invariant;
+    address public tokenAddress;
+    
+    ERC20Interface token;
+
+    /// MODIFIERS
+    modifier exchangeInitialized() {
+        require(invariant > 0);
+        _;
+    }
+
+    /// CONSTRUCTOR
+    constructor (address _tokenAddress) public {
+        tokenAddress = _tokenAddress;
+        token = ERC20Interface(tokenAddress);
+    }
+
+    /// FALLBACK FUNCTION
+    function() public payable {
+        require(msg.value != 0);
+        ethToToken(msg.sender, msg.sender, msg.value, 1);
+    }
+
+    /// EXTERNAL FUNCTIONS
+    function init(uint256 _tokenAmount) external payable {
+        require(invariant == 0);
+        
+        ethPool = msg.value;
+        tokenPool = _tokenAmount;
+        invariant = ethPool.mul(tokenPool);
+        require(token.transferFrom(msg.sender, address(this), _tokenAmount));
+    }
+    
+    function kill() public onlyOwner {
+        selfdestruct(msg.sender);
+    }
+    
+    function withdrawTokens() public onlyOwner returns (bool success) {
+        uint256 balance = token.balanceOf(this);
+
+        // Double checking
+        require(balance > 0);
+        require(token.transfer(msg.sender, balance));
+        return true;
+    }
+
+    // Buyer swaps ETH for Tokens
+    function ethToTokenSwap(
+        uint256 _minTokens,
+        uint256 _timeout
+    )
+        external
+        payable
+    {
+        require(msg.value > 0 && _minTokens > 0 && now < _timeout);
+        ethToToken(msg.sender, msg.sender, msg.value,  _minTokens);
+    }
+
+
+    // Buyer swaps Tokens for ETH
+    function tokenToEthSwap(
+        uint256 _tokenAmount,
+        uint256 _minEth,
+        uint256 _timeout
+    )
+        external
+    {
+        require(_tokenAmount > 0 && _minEth > 0 && now < _timeout);
+        tokenToEth(msg.sender, msg.sender, _tokenAmount, _minEth);
+    }
+
+    /// INTERNAL FUNCTIONS
+    function ethToToken(
+        address buyer,
+        address recipient,
+        uint256 ethIn,
+        uint256 minTokensOut
+    )
+        internal
+        exchangeInitialized
+    {
+        uint256 fee = ethIn.div(FEE_RATE);
+        uint256 newEthPool = ethPool.add(ethIn);
+        uint256 tempEthPool = newEthPool.sub(fee);
+        uint256 newTokenPool = invariant.div(tempEthPool);
+        uint256 tokensOut = tokenPool.sub(newTokenPool);
+        require(tokensOut >= minTokensOut && tokensOut <= tokenPool);
+        ethPool = newEthPool;
+        tokenPool = newTokenPool;
+        invariant = newEthPool.mul(newTokenPool);
+        emit EthToTokenPurchase(buyer, ethIn, tokensOut);
+        require(token.transfer(recipient, tokensOut));
+    }
+
+    function tokenToEth(
+        address buyer,
+        address recipient,
+        uint256 tokensIn,
+        uint256 minEthOut
+    )
+        internal
+        exchangeInitialized
+    {
+        uint256 fee = tokensIn.div(FEE_RATE);
+        uint256 newTokenPool = tokenPool.add(tokensIn);
+        uint256 tempTokenPool = newTokenPool.sub(fee);
+        uint256 newEthPool = invariant.div(tempTokenPool);
+        uint256 ethOut = ethPool.sub(newEthPool);
+        require(ethOut >= minEthOut && ethOut <= ethPool);
+        tokenPool = newTokenPool;
+        ethPool = newEthPool;
+        invariant = newEthPool.mul(newTokenPool);
+        emit TokenToEthPurchase(buyer, tokensIn, ethOut);
+        require(token.transferFrom(buyer, address(this), tokensIn));
+        recipient.transfer(ethOut);
+    }
+}
---
+++ contracts/UnstoppablePaymentGateway/UnstoppablePaymentGateway.sol
@@ -0,0 +1,136 @@
+pragma solidity 0.4.25;
+contract Ownable {
+    address private _owner;
+
+    event OwnershipTransferred(
+        address indexed previousOwner,
+        address indexed newOwner
+    );
+
+    /**
+    * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+    * account.
+    */
+    constructor() public {
+        _owner = msg.sender;
+        emit OwnershipTransferred(address(0), _owner);
+    }
+
+    /**
+    * @return the address of the owner.
+    */
+    function owner() public view returns(address) {
+        return _owner;
+    }
+
+    /**
+    * @dev Throws if called by any account other than the owner.
+    */
+    modifier onlyOwner() {
+        require(isOwner());
+        _;
+    }
+
+    /**
+    * @return true if `msg.sender` is the owner of the contract.
+    */
+    function isOwner() public view returns(bool) {
+        return msg.sender == _owner;
+    }
+
+    /**
+    * @dev Allows the current owner to relinquish control of the contract.
+    * @notice Renouncing to ownership will leave the contract without an owner.
+    * It will not be possible to call the functions with the `onlyOwner`
+    * modifier anymore.
+    */
+    function renounceOwnership() public onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+
+    /**
+    * @dev Allows the current owner to transfer control of the contract to a newOwner.
+    * @param newOwner The address to transfer ownership to.
+    */
+    function transferOwnership(address newOwner) public onlyOwner {
+        _transferOwnership(newOwner);
+    }
+
+    /**
+    * @dev Transfers control of the contract to a newOwner.
+    * @param newOwner The address to transfer ownership to.
+    */
+    function _transferOwnership(address newOwner) internal {
+        require(newOwner != address(0));
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+}
+
+interface IERC20 {
+
+    function totalSupply() public constant returns (uint);
+
+    function balanceOf(address tokenOwner) public constant returns (uint balance);
+
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
+
+    function transfer(address to, uint tokens) public returns (bool success);
+
+    function approve(address spender, uint tokens) public returns (bool success);
+
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+
+
+    event Transfer(address indexed from, address indexed to, uint tokens);
+
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+
+}
+
+
+contract GlobalVar {
+    /**
+    * Address Exchanges
+    */
+    address public StupidExchange = 0xb6f448e57c4b01ea88fa04a92b7696871bf12c61;
+    
+    address public GOToken = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    address public Pizzatoken = 0x3553a51d08db0565fe4bf8cd4dd554f9f24a65bc;
+    address public USDGtoken = 0xd1574837a02ba934be92adbde12a62a658cd3186;
+}
+
+contract UnstoppablePaymentGateway is GlobalVar, Ownable{
+    
+    
+    struct PaymentObj {
+      address _payer; 
+      address seller;
+      address _token;
+      uint _amount; 
+      bytes32 _data;
+    }
+    
+    event ProofOfPayment(address indexed _payer, address indexed seller, address _token, uint _amount, bytes32 _data);
+    
+    mapping(address => mapping(uint => PaymentObj)) public payment;
+    
+    function payWithGO(address seller, uint _orderId) internal returns  (bool success){
+      require(seller != address(0)); 
+      require(msg.value > 0);
+      
+      seller.transfer(msg.value);
+      
+      bytes32 data = keccak256(abi.encodePacked( seller,_orderId ) );
+          
+      payment[seller][_orderId] = PaymentObj(msg.sender, seller, GOToken, msg.value, data);
+      emit ProofOfPayment(msg.sender, seller, GOToken, msg.value, data);
+      return true;
+    }
+    
+    function kill() public onlyOwner {
+        selfdestruct(msg.sender);
+    }
+    
+}
